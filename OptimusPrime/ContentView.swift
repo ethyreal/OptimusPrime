f2b0e384e7bb3c3e030e6b4b095d7c46a9f8aee7
George Webster <george@ethyreal.com>
2020-02-14 09:30:46 -0800

    Initial Commit

---
+++ OptimusPrime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OptimusPrime
+//
+//  Created by George Webster on 2/14/20.
+//  Copyright © 2020 George Webster. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
